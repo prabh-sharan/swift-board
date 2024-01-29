35f57a9ccf23e4be77d1d848574a8a8e9ca19383
cpop <prabhsharan971@gmail.com>
2024-01-29 16:50:32 +0530

    tools-functionality

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let optionsCont=document.querySelector(".options-cont");
 let toolsCont=document.querySelector(".tools-cont");
+let optionsCont=document.querySelector(".options-cont");
+let optionsFlag=true;
+
 let pencilToolCont=document.querySelector(".pencil-tool-cont");
 let eraserToolCont=document.querySelector(".eraser-tool-cont");
-let optionsFlag=true;
+
+let pencil=document.querySelector(".pencil");
+let eraser=document.querySelector(".eraser");
+let pencilFlag=false;
+let eraserFlag=false;
 
 // true->show tools ,false->hide tools bar
 optionsCont.addEventListener("click", (e)=>{
@@ -14,17 +20,33 @@ optionsCont.addEventListener("click", (e)=>{
 
 function openTools(){
     let iconElem = optionsCont.children[0];
-    iconElem.classList.remove("fa-x");
-    iconElem.classList.add("fa-bars")
+    iconElem.classList.remove("fa-bars");
+    iconElem.classList.add("fa-x")
     toolsCont.style.display="flex"
 }
 
 function closeTools(){
     let iconElem = optionsCont.children[0];
-    iconElem.classList.remove("fa-bars");
-    iconElem.classList.add("fa-x");
+    iconElem.classList.remove("fa-x");
+    iconElem.classList.add("fa-bars");
     toolsCont.style.display="none";
 
     pencilToolCont.style.display="none";
     eraserToolCont.style.display="none";
 }
+
+// true->show pencil tool , false-hide pencil tool
+pencil.addEventListener("click", (e)=>{
+    pencilFlag=!pencilFlag;
+    
+    if(pencilFlag) pencilToolCont.style.display="block";
+    else pencilToolCont.style.display="none";
+})
+
+// true->show eraser tool , false-hide eraser tool features
+eraser.addEventListener("click", (e)=>{
+    eraserFlag=!eraserFlag;
+
+    if(eraserFlag) eraserToolCont.style.display="flex";
+    else eraserToolCont.style.display="none";
+})
