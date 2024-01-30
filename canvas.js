7ee5eccbd985713755b049324fce849d23b1a8e9
cpop <prabhsharan971@gmail.com>
2024-01-31 00:35:09 +0530

    pencolor-func

--- canvas.js
+++ canvas.js
@@ -1,25 +1,59 @@
 let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-let mousedown = false;
+let mouseDown = false;
+
+let pencilColor = document.querySelectorAll(".pencil-color");
+let pencilWidthElem = document.querySelector(".pencil-width");
+let eraserWidthElem = document.querySelector(".eraser-width");
+
+let penColor = "red";
+let penWidth = pencilWidthElem.value;
+let eraserWidth = eraserWidthElem.value;
+let eraserColor = "white";
 
 let tool = canvas.getContext("2d");
 
-tool.strokeStyle = "red";
-tool.lineWidth = "3";
+tool.strokeStyle = penColor; 
+tool.lineWidth = penWidth;
 
 //mousedown -> begin new path , mousemove-> fill stroke color 
 canvas.addEventListener("mousedown", (e)=> {
-    mousedown = !mousedown;
-    tool.beginPath(); //new path
-    tool.moveTo(e.clientX,e.clientY); // start pt
+    mouseDown = !mouseDown;
+    beginPath({
+        x: e.clientX,
+        y: e.clientY
+    })
     
 })
 
 canvas.addEventListener("mousemove", (e)=> {
-    mousedown = !mousedown;
-    if(mousedown){
-        tool.lineTo(e.clientX,e.clientY); //end pt
-        tool.stroke();  //fill color   
-    }
+    if(mouseDown) drawStroke({
+        x: e.clientX,
+        y: e.clientY
+    })  
+    
+})
+
+canvas.addEventListener("mouseup", (e)=>{
+     mouseDown =!mouseDown;
+})
+
+function beginPath(strokeObj){
+    tool.beginPath(); //new path
+    tool.moveTo(strokeObj.x,strokeObj.y); // start pt
+}
+function drawStroke(strokeObj){
+    tool.lineTo(strokeObj.x,strokeObj.y); //end pt
+    tool.stroke();  //fill color   
+}
+
+// accessing colors and using click event listener
+pencilColor.forEach((colorElem) =>{
+    colorElem.addEventListener("click", (e) =>{
+        let color = colorElem.classList[0];
+        // console.log(color)
+        penColor = color;
+        tool.strokeStyle = penColor;
+    })
 })
